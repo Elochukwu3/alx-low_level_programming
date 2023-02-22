950dbde261a1b9d5151fb9137749022842d0d37a
Elochukwu3 <oguajuromauld5@.com>
2023-02-22 01:32:15 -0800

    Add task10

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - execute add function
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
+
